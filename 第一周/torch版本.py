52b155cecccff71dae83a80810a126b983053d17
keray <1172304645@qq.com>
2025-08-31 19:31:36 +0800

    cuda依赖

--- 第一周/torch版本.py
+++ 第一周/torch版本.py
@@ -1,3 +1,6 @@
 import torch
 print(torch.__version__)
 print(torch.mps.is_available())
+if torch.cuda.is_available():
+    print(torch.version.cuda)  # PyTorch 编译的 CUDA 版本
+    print(torch.cuda.get_device_name(0))  # GPU 名称
